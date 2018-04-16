10e6c1636aaeca7da479fe827dcc313bcaa55055
Leonardo Souza <leonardostsouza@gmail.com>
2018-04-16 20:07:12 -0300

    Create source file for node_vector.h

---
+++ node_vector.c
@@ -0,0 +1,19 @@
+#include "headers/node_vector.h"
+
+static node_t **node_vector = NULL;
+
+void addNode(node_t *node){
+
+}
+
+node_t getNode(size_t index){
+
+}
+
+void modifyEdge(size_t origNodeIndex, size_t destNodeIndex, port_t origPort, port_t destPort){
+
+}
+
+void removeNode(size_t nodeIndex){
+    
+}
