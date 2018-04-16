e6c89a5c1d2b0398e39ad3d9c157cf27ce54aad0
Leonardo Souza <leonardostsouza@gmail.com>
2018-04-16 20:04:09 -0300

    Add first C files

---
+++ c_src/headers/interaction-combinators.h
@@ -0,0 +1,44 @@
+// Implements symmetric interaction combinators with infinite node colors.
+
+void wire(/*node, port*/){
+
+}
+
+void node(/*wire*/){
+
+}
+
+void port(/*wire*/){
+
+}
+
+void flip(/*mem, w*/) {
+
+}
+
+void Node(/*mem, kind*/) {
+
+}
+
+void kind(/*mem, node*/) {
+
+}
+
+void meta(/*mem, node*/) {
+
+}
+
+void setMeta(/*mem, node, meta*/) {
+}
+
+void link(/*mem, a, b*/) {
+
+}
+
+void reduce(/*net*/) {
+
+}
+
+void rewrite(/*mem, x, y, stats*/) {
+
+}
---
+++ c_src/main.c
@@ -0,0 +1,7 @@
+#include <stdint.h>
+#include <stdio.h>
+//#include "lambda-calculus.h"
+
+int main(int argc, char *argv[]){
+    return 0;
+}
