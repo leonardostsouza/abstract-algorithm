05e02ee23fd0e2f023ccebf124b2b562667d13fa
Leonardo Souza <leonardostsouza@gmail.com>
2018-04-18 15:48:02 -0300

    Explanation about the buffer implementation in iteraction-combinators.h

--- c_src/headers/interaction-combinators.h
+++ c_src/headers/interaction-combinators.h
@@ -1,5 +1,35 @@
 // Implements symmetric interaction combinators with infinite node colors.
 
+// A node is composed of 4 uin32_t values.
+// It has the format:
+// PORT_0 | PORT_1 | PORT_2 | KIND
+//
+//      KIND -> A number wich represents the kind of the node.
+//              This value is important for graph reduction.
+//              The 30 Most Significant Bits (MSB) represent the 'Kind'
+//              number and the 2 Least Significant Bits (LSB) represent the Met.
+//
+//              Meta is not an important value by itself. It is only used by the
+//              reduce and rewrite functions to mark nodes it has already visited
+//
+//      PORT_X-> Represents a link with another node by port X. Each node
+//               has exactly 3 pots. Each of them links to a port in a
+//               different node. This position in memory holds the foreign port
+//               index in the buffer.
+
+// The buffer is composed by an array of uint32_t values. The last value in the
+// array holds the index to the last "free" prosition it has on record. The buffers
+// has the following format:
+//
+// NODE_1 | NODE_2 | ... | NODE_n | next
+//
+//      NODE_X ->   The representation of a node in memory. Remember each node
+//                  has the format PORT_0 | PORT_1 | PORT_2 | KIND
+//
+//      next ->     The last position of the buffer. Holds the next free index
+//                  in which the buffer can be written.
+//
+
 #ifndef __INTERACTION_COMBINATORS_H__
 #define __INTERACTION_COMBINATORS_H__
 
@@ -8,12 +38,13 @@
 //#include <stddef.h>   // <---- Need to uncomment this line if buffet_t changes
                         // to size_t* type
 
-#define MAX_NODES       10
+#define MAX_NODES       1024
 #define NODE_SIZE       4   // Do not change this value!
                             // Each node must have 1 KIND and 3 ports for the
                             // evaluator to run optimaly
 
-#define BUFFER_MAX_SIZE     (MAX_NODES * NODE_SIZE)
+#define MAX_BUFFER_SIZE (MAX_NODES * NODE_SIZE)
+#define NEXT            MAX_BUFFER_SIZE
 
 typedef enum {PORT_0 = 0, PORT_1, PORT_2} port_t;
 typedef uint32_t kind_t;    // This value is shifted to the left so the 2 LSB may
@@ -21,12 +52,8 @@ typedef uint32_t kind_t;    // This value is shifted to the left so the 2 LSB ma
 typedef uint8_t meta_t      // only the 2 LSB are relevant
 typedef uint32_t index_t    // Maybe will need to change to size_t* in the future
                             // in order to support bigger buffers.
-/*typedef struct {
-    index_t next; // first "never writen" buffer position
-    index_t buffer[BUFFER_MAX_SIZE];
-} buffer_t;*/
-typedef index_t* buffer_t;
 
+typedef index_t buffer_t[MAX_BUFFER_SIZE + 1]
 
 // Allocs memory to buffer and writes 0 to all positions
 void bufferInit(buffer_t);
